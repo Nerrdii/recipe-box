318305e5d678062af3187b1b5609375653ece4cc
Joseph Davis <joseph@josephndavis.com>
2018-05-16 16:43:00 -0500

    Add recipe filter

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -3,9 +3,16 @@ import Recipe from "./Recipe";
 import "./css/Recipes.css";
 
 class Recipes extends Component {
-  state = {
-    recipes: []
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      initialRecipes: [],
+      recipes: []
+    };
+
+    this.filterList = this.filterList.bind(this);
+  }
 
   componentDidMount() {
     this.getRecipes();
@@ -15,11 +22,24 @@ class Recipes extends Component {
     fetch("/api/recipes")
       .then(res => res.json())
       .then(data => {
+        this.setState({ initialRecipes: data });
         this.setState({ recipes: data });
       })
       .catch(err => {
         console.log(err);
       });
+  }
+
+  filterList(e) {
+    let updatedList = this.state.initialRecipes;
+
+    updatedList = updatedList.filter(recipe => {
+      return (
+        recipe.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+      );
+    });
+
+    this.setState({ recipes: updatedList });
   }
 
   render() {
@@ -37,6 +57,15 @@ class Recipes extends Component {
     return (
       <div>
         <h1>Recipes</h1>
+        <div className="input-field">
+          <i className="material-icons prefix">search</i>
+          <input
+            type="text"
+            id="searchTerm"
+            onChange={this.filterList}
+            placeholder="Search..."
+          />
+        </div>
         {recipes}
       </div>
     );
