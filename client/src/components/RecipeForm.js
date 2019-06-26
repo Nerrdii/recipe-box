0c07156311ffd1b946654673c6baba5ca05b0cc3
Joseph Davis <joseph.davis@hrblock.com>
2019-06-26 10:26:29 -0500

    Refactor recipe form

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,156 +1,217 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import { Formik, Field, FieldArray, Form as FormikForm } from 'formik';
-import ErrorMessage from './ErrorMessage';
+import { Formik, FieldArray, ErrorMessage } from 'formik';
 import schema from '../config/schema.js';
 
-class RecipeForm extends Component {
-  renderFieldArray = (values, field, label, handleChange, handleBlur) => {
-    return (
-      <FieldArray
-        name={field}
-        id={field}
-        render={arrayHelpers => (
-          <React.Fragment>
-            <div className="d-flex">
-              <h4 className="mr-3">{label}s</h4>
-              <Button
-                type="button"
-                variant="primary"
-                className="mb-3"
-                onClick={() => arrayHelpers.push('')}>
-                Add
-              </Button>
-            </div>
-            {values && values.length > 0 ? (
-              values.map((v, index) => (
-                <Form.Group key={index}>
-                  <Form.Label htmlFor={`${field}.${index}`}>
-                    {label} #{index + 1}
-                  </Form.Label>
-                  <InputGroup>
-                    <Field
-                      type="text"
-                      name={`${field}.${index}`}
-                      id={`${field}.${index}`}
-                      className="form-control"
-                      values={values[index]}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    <InputGroup.Append>
-                      <Button
-                        type="button"
-                        variant="danger"
-                        onClick={() => arrayHelpers.remove(index)}>
-                        Remove
-                      </Button>
-                    </InputGroup.Append>
-                  </InputGroup>
-                  <ErrorMessage name={`${field}.${index}`} />
-                </Form.Group>
-              ))
-            ) : (
-              <ErrorMessage name={field} />
-            )}
-          </React.Fragment>
-        )}
-      />
-    );
-  };
+const RecipeForm = ({ initialValues, cancelLink, onSubmit }) => (
+  <Formik
+    initialValues={initialValues}
+    onSubmit={values => onSubmit(values)}
+    validationSchema={schema}
+    render={({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      isSubmitting,
+      handleReset,
+      dirty
+    }) => (
+      <Form className="my-4" onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label htmlFor="name">Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isInvalid={touched.name && errors.name}
+          />
+          <Form.Control.Feedback type="invalid">
+            <ErrorMessage name="name" />
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="description">Description</Form.Label>
+          <Form.Control
+            type="text"
+            name="description"
+            id="description"
+            className="form-control"
+            value={values.description}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="servings">Servings</Form.Label>
+          <Form.Control
+            type="number"
+            name="servings"
+            id="servings"
+            className="form-control"
+            value={values.servings}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isInvalid={touched.servings && errors.servings}
+          />
+          <Form.Control.Feedback type="invalid">
+            <ErrorMessage name="servings" />
+          </Form.Control.Feedback>
+        </Form.Group>
+        <FieldArray
+          name="ingredients"
+          id="ingredients"
+          render={arrayHelpers => {
+            const { ingredients } = values;
 
-  render() {
-    return (
-      <Formik
-        initialValues={this.props.initialValues}
-        enableReinitialize={true}
-        onSubmit={values => this.props.onSubmit(values)}
-        validationSchema={schema}
-        render={({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          isSubmitting,
-          handleReset,
-          dirty
-        }) => (
-          <FormikForm className="my-4">
-            <Form.Group>
-              <Form.Label htmlFor="name">Name</Form.Label>
-              <Field
-                type="text"
-                name="name"
-                id="name"
-                className="form-control"
-                value={values.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              {touched.name && errors.name && errors.name}
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="description">Description</Form.Label>
-              <Field
-                type="text"
-                name="description"
-                id="description"
-                className="form-control"
-                value={values.description}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="servings">Servings</Form.Label>
-              <Field
-                type="number"
-                name="servings"
-                id="servings"
-                className="form-control"
-                value={values.servings}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Group>
-            {this.renderFieldArray(
-              values.ingredients,
-              'ingredients',
-              'Ingredient',
-              handleChange,
-              handleBlur
-            )}
-            {this.renderFieldArray(
-              values.directions,
-              'directions',
-              'Direction',
-              handleChange,
-              handleBlur
-            )}
-            <div className="mt-5">
-              {this.props.cancelLink}
-              <div className="float-right">
-                <Button
-                  type="button"
-                  variant="secondary"
-                  className="mr-3"
-                  onClick={handleReset}
-                  disabled={!dirty || isSubmitting}>
-                  Reset
-                </Button>
-                <Button type="submit" variant="primary" disabled={isSubmitting}>
-                  Submit
-                </Button>
-              </div>
-            </div>
-          </FormikForm>
-        )}
-      />
-    );
-  }
-}
+            return (
+              <React.Fragment>
+                <div className="d-flex">
+                  <h4 className="mr-3">Ingredients</h4>
+                  <Button
+                    type="button"
+                    variant="primary"
+                    className="mb-3"
+                    onClick={() => arrayHelpers.push('')}>
+                    Add
+                  </Button>
+                </div>
+                {ingredients && ingredients.length > 0 ? (
+                  ingredients.map((ingredient, index) => (
+                    <Form.Group key={index}>
+                      <Form.Label htmlFor={`ingredients[${index}]`}>
+                        Ingredient #{index + 1}
+                      </Form.Label>
+                      <InputGroup>
+                        <Form.Control
+                          type="text"
+                          name={`ingredients[${index}]`}
+                          id={`ingredients[${index}]`}
+                          className="form-control"
+                          value={ingredient}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          isInvalid={
+                            touched.ingredients &&
+                            touched.ingredients[index] &&
+                            errors.ingredients &&
+                            errors.ingredients[index]
+                          }
+                        />
+                        <InputGroup.Append>
+                          <Button
+                            type="button"
+                            variant="danger"
+                            onClick={() => arrayHelpers.remove(index)}>
+                            &times;
+                          </Button>
+                        </InputGroup.Append>
+                        <Form.Control.Feedback type="invalid">
+                          <ErrorMessage name={`ingredients.${index}`} />
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                  ))
+                ) : (
+                  <Alert variant="danger">
+                    <ErrorMessage name="ingredients" />
+                  </Alert>
+                )}
+              </React.Fragment>
+            );
+          }}
+        />
+        <FieldArray
+          name="directions"
+          id="directions"
+          render={arrayHelpers => {
+            const { directions } = values;
+
+            return (
+              <React.Fragment>
+                <div className="d-flex">
+                  <h4 className="mr-3">Directions</h4>
+                  <Button
+                    type="button"
+                    variant="primary"
+                    className="mb-3"
+                    onClick={() => arrayHelpers.push('')}>
+                    Add
+                  </Button>
+                </div>
+                {directions && directions.length > 0 ? (
+                  directions.map((direction, index) => (
+                    <Form.Group key={index}>
+                      <Form.Label htmlFor={`directions[${index}]`}>
+                        Direction #{index + 1}
+                      </Form.Label>
+                      <InputGroup>
+                        <Form.Control
+                          type="text"
+                          name={`directions[${index}]`}
+                          id={`directions[${index}]`}
+                          className="form-control"
+                          values={direction}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          isInvalid={
+                            touched.directions &&
+                            touched.directions[index] &&
+                            errors.directions &&
+                            errors.directions[index]
+                          }
+                        />
+                        <InputGroup.Append>
+                          <Button
+                            type="button"
+                            variant="danger"
+                            onClick={() => arrayHelpers.remove(index)}>
+                            &times;
+                          </Button>
+                        </InputGroup.Append>
+                        <Form.Control.Feedback type="invalid">
+                          <ErrorMessage name={`directions.${index}`} />
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                  ))
+                ) : (
+                  <Alert variant="danger">
+                    <ErrorMessage name="directions" />
+                  </Alert>
+                )}
+              </React.Fragment>
+            );
+          }}
+        />
+        <div className="mt-5">
+          {cancelLink}
+          <div className="float-right">
+            <Button
+              type="button"
+              variant="secondary"
+              className="mr-3"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}>
+              Reset
+            </Button>
+            <Button type="submit" variant="primary" disabled={isSubmitting}>
+              Submit
+            </Button>
+          </div>
+        </div>
+      </Form>
+    )}
+  />
+);
 
 export default RecipeForm;
--- client/src/config/schema.js
+++ client/src/config/schema.js
@@ -1,13 +1,16 @@
-import { object, array, string } from 'yup';
+import { array, number, object, string } from 'yup';
 
 const schema = object().shape({
   name: string().required('Name is required'),
+  servings: number()
+    .required('Servings is required')
+    .min(1, 'Servings cannot be less than 1'),
   ingredients: array()
     .of(string().required('Ingredient is required'))
-    .required('Ingredients are required'),
+    .required('At least one ingredient is required'),
   directions: array()
     .of(string().required('Direction is required'))
-    .required('Directions are required')
+    .required('At least one direction is required')
 });
 
 export default schema;
