34423037f085b886c59ae02e55c577e44a4c02e6
Joseph Davis <joseph.davis@hrblock.com>
2019-06-26 10:26:41 -0500

    Change App to functional component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container';
@@ -9,24 +9,20 @@ import EditRecipe from './EditRecipe';
 import RecipeDetails from './RecipeDetails';
 import RedirectComponent from './RedirectComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <Container className="mt-4">
-          <Switch>
-            <Route exact path="/" component={RecipeList} />
-            <Route exact path="/recipes/add" component={AddRecipe} />
-            <Route path="/recipes/:id/edit" component={EditRecipe} />
-            <Route path="/recipes/:id" component={RecipeDetails} />
-            <Route path="/redirect" component={RedirectComponent} />
-          </Switch>
-        </Container>
-      </React.Fragment>
-    );
-  }
-}
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Container className="mt-4">
+      <Switch>
+        <Route exact path="/" component={RecipeList} />
+        <Route exact path="/recipes/add" component={AddRecipe} />
+        <Route path="/recipes/:id/edit" component={EditRecipe} />
+        <Route path="/recipes/:id" component={RecipeDetails} />
+        <Route path="/redirect" component={RedirectComponent} />
+      </Switch>
+    </Container>
+  </React.Fragment>
+);
 
 const mapStateToProps = ({ auth }) => {
   return { loggedIn: auth.loggedIn };
