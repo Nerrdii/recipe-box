6d4cb1c7213d0ee86d8d10db2a70d3f1af88f6c2
Joseph Davis <joseph.n.davis.98@gmail.com>
2023-11-04 14:28:55 -0500

    Update server deps

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const passport = require('passport');
 require('dotenv').config();
 require('./services/passport');
 
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true },
-  () => console.log('Connected to MongoDB')
-);
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 
